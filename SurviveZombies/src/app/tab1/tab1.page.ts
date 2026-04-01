1dcc33d1cefed769c4b8e588e329dff3a6004b76
Shreyas <helloshreyasj@gmail.com>
2026-04-01 17:55:40 +0100

    feat: add items to stock

--- SurviveZombies/src/app/tab1/tab1.page.ts
+++ SurviveZombies/src/app/tab1/tab1.page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { IonButton, IonList, IonItem, IonSegmentContent, IonSegmentView,
           IonLabel, IonSegment, IonSegmentButton, IonHeader,
-          IonToolbar, IonTitle, IonContent, IonBadge, IonInput } from '@ionic/angular/standalone';
-
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { RouterLink } from '@angular/router';
+          IonToolbar, IonTitle, IonContent, IonBadge, IonInput, IonText, IonAlert } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
+import type { OverlayEventDetail } from '@ionic/core';
+
 
 export interface Item  {
   name: string;
@@ -17,25 +16,34 @@ export interface Item  {
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent,
-    ExploreContainerComponent, IonLabel, IonSegment,
-    IonSegmentButton, RouterLink, IonSegmentView,
-    IonSegmentContent, IonList, IonItem, IonBadge, IonInput, IonButton],
+  imports: [IonAlert, IonText, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonSegment,
+    IonSegmentButton, IonSegmentView,
+    IonSegmentContent, IonList, IonItem, IonBadge, IonInput, IonButton ],
 })
 
 export class Tab1Page {
   constructor() {}
 
-  itemName: string = "";
-  itemQty: number = 0;
-  itemDesc: string = "";
+  alertButtons = ['OK'];
+  itemName: string = "No name provided";
+  itemQty: number = 1;
+  itemDesc?: string = "No description provided";
+  quantityAlert: boolean = false;
 
   items: Item[] = [
   ];
   itemCount:number = this.items.length;
 
   addToStock = () => {
+    if (this.itemQty <= 0) {
+      this.quantityAlert = true;
+      return;
+    }
     this.items.push({"name": this.itemName, "qty": this.itemQty, "desc": this.itemDesc});    
     this.itemCount++;
+  }
+
+  setResult(event: CustomEvent<OverlayEventDetail>) {
+    this.quantityAlert = false;
   }
 }
