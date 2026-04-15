42290ca14ed358d5ba739be9653722b3789f02d2
Shreyas <helloshreyasj@gmail.com>
2026-04-15 14:23:09 +0100

    feat: show link to api key when entry is in focus

--- SurviveZombies/src/app/settings/settings/settings.page.ts
+++ SurviveZombies/src/app/settings/settings/settings.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonInput, IonItem, IonIcon, IonButton, IonToggle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonInput, IonItem, IonIcon, IonButton, IonToggle, IonText } from '@ionic/angular/standalone';
 import { add } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { Storage } from '@ionic/storage-angular';
@@ -12,7 +12,7 @@ import { Storage } from '@ionic/storage-angular';
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonToggle, IonButton, IonIcon, IonItem, IonInput, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton ]
+  imports: [IonText, IonToggle, IonButton, IonIcon, IonItem, IonInput, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton ]
 })
 export class SettingsPage implements OnInit {
 
@@ -25,8 +25,19 @@ export class SettingsPage implements OnInit {
   }
 
   apiKey: string = "";
+  isFocusedApiBox: boolean = false;
 
   setApiKey = async () => {
     await this.storage.set('api_key', this.apiKey);
+  }
+
+  onFocusApiBox = () => {
+    this.isFocusedApiBox = true;
+  }
+
+  onBlurApiBox = async () => {
+    await setTimeout(() => {
+      this.isFocusedApiBox = false;
+    }, 120);
   }
 }
